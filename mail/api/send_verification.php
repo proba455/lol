2e3516bac27b729f07216b3404a14a7442a66ad5
proba455 <domadima455@gmail.com>
2026-03-11 20:08:37 +0500

    Update send_verification.php

--- mail/api/send_verification.php
+++ mail/api/send_verification.php
@@ -19,9 +19,9 @@ function send_json($payload, $code = 200)
 const SMTP_HOST       = 'smtp.gmail.com';
 const SMTP_PORT       = 587;
 const SMTP_USERNAME   = 'shubkagames@gmail.com';
-const SMTP_PASSWORD   = 'ancd snwg gmaj ozf';
+const SMTP_PASSWORD   = 'mvnpwewzwqrtsqiu';
 const SMTP_FROM_EMAIL = 'shubkagames@gmail.com';
-const SMTP_FROM_NAME  = 'Games';
+const SMTP_FROM_NAME  = 'Nexules';
 
 set_error_handler(function ($severity, $message, $file, $line) {
     throw new ErrorException($message, 0, $severity, $file, $line);
@@ -145,5 +145,6 @@ exit;
     send_json(['ok' => false, 'error' => 'server_exception', 'detail' => $e->getMessage()], 500);
     exit;
 }
+
 
 
